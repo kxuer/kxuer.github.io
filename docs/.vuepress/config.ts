e82fc7080a97a18854a5458faf9cce862ef790f4
zqw <qw@a>
2022-11-04 19:22:51 +0800

    整理03.技术

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -42,7 +42,6 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: '浏览器', link: '/frontend/browser/' },
           { text: 'ES6', link: '/frontend/es6/' },
           { text: '框架', link: '/frontend/framework/' },
-          { text: '使用技巧', link: '/frontend/tips/' },
           { text: '他人文章', link: '/frontend/others/' },
         ],
       },
@@ -57,10 +56,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         text: '技术',
         link: '/technology/',
         items: [
-          { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-          { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-          { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-          { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+          { text: '技术文档', link: '/technology/tech/' },
+          { text: '博客搭建', link: '/technology/blog/' },
+          { text: '软件使用', link: '/technology/software/' },
         ],
       },
       {
